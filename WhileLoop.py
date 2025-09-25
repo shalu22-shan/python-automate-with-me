9542b06d90efc51c8c94f9ef0d6e67de9bc9fc99
cynthia <shalugregory@gmail.com>
2025-09-25 21:57:13 +0100

    while loop

---
+++ WhileLoop.py
@@ -0,0 +1,11 @@
+What is a while loop?
+
+A while loop keeps running as long as the condition is true.
+
+Sum of first 10 natural numbers
+i = 1
+sum = 0
+while i <= 10:
+    sum = sum + i
+    i = i + 1
+print("Sum =", sum)
