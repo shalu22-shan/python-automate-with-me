b95ba05758a138495b99720ee14a71fa87d1d13f
cynthia <shalugregory@gmail.com>
2025-09-25 21:54:21 +0100

    for loop

---
+++ ForLoop.py
@@ -0,0 +1,14 @@
+What is a for loop?
+
+A for loop in Python is used to repeat a block of code a certain number of times.
+
+It works with sequences like range, lists, strings, tuples, etc.
+
+pattern is
+for variable in range(start, end):
+
+    sum of 2 numbers using for loop
+        sum = 0
+        for i in range(1, 6):
+            sum = sum + i
+        print("Sum =", sum)
